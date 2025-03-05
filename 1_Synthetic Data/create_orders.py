cc89a77b257844459c9f19008a8c1ee2fdbbfac3
Anthony-Ung-931 <makeanthonygreatagain@gmail.com>
2025-03-05 17:23:51 -0500

    Created pseudocode for generation of JSON

---
+++ 1_Synthetic Data/create_orders.py
@@ -0,0 +1,48 @@
+'''
+Pseudocode for order generation
+'''
+class ARGS:
+    SAVE_CUSTOMERS = True
+    SAVE_PRODUCTS = True
+
+
+'''
+Global variables
+'''
+customers = []
+products = []
+orders = []
+
+
+class Customer:
+    def __init__(self, **kwargs):
+        pass
+
+
+class Product:
+    def __init__(self, **kwargs):
+        pass
+
+
+class Order:
+    def __init__(self):
+        '''
+            Logic for order creation is as follows:
+            Select a customer at random
+            Select 1 or more items at random
+            Set the quantity of some items (rare) higher than 1.
+        '''
+        pass
+
+
+'''
+    This will populate 3 separate JSON files from 3 separate arrays.
+'''
+def build_json():
+    pass
+
+def run():
+    pass
+
+
+run()
